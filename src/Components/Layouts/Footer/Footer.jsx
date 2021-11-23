60df2ce626ea61292be85a6ffa1dbbad40e7b3b6
clperez029 <88810234+clperez029@users.noreply.github.com>
2021-11-22 22:26:57 -0500

    Se agrega creditos en Pie de pagina

--- src/Components/Layouts/Footer/Footer.jsx
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './Footer.css';
 import { Socialmedia } from "../../UI/Socialmedia/Socialmedia";
+import { Credits } from "../../UI/Credits/Credits";
 
 export const Footer = () => {
     return (
         <div className="cont-footer">
             <Socialmedia/>
+            <Credits/>
         </div>
     )
 }
---
+++ src/Components/UI/Credits/Credits.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Credits.css';
+
+export const Credits = () => {
+    return (
+        <>
+            <p className="text-credits">Desarrollado por: <br/>
+            Cristian Leandro Perez Pelaez - ADSI 2184066</p>
+            
+        </>
+    )
+}
